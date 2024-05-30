aacd68e72adbe98dd534e005a5a923ec139d394c
sidex15 <moses.limbo@gmail.com>
2024-05-30 12:03:40 +0800

    Change clang 11 toolchain to clang 13

--- build_flash_googleclang_quick.sh
+++ build_flash_googleclang_quick.sh
@@ -7,13 +7,13 @@ echo
 mkdir -p out
 export ARCH=arm64
 export SUBARCH=arm64
-export CLANG_PATH=~/toolchains/android_prebuilts_clang_host_linux-x86_clang-6443078/bin
+export CLANG_PATH=~/toolchains/clang-r428724/bin
 export PATH=${CLANG_PATH}:${PATH}
 export DTC_EXT=/usr/bin/dtc
 export CLANG_TRIPLE=aarch64-linux-gnu-
 export CROSS_COMPILE=~/toolchains/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9/bin/aarch64-linux-android-
 export CROSS_COMPILE_ARM32=~/toolchains/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9/bin/arm-linux-androideabi-
-export LD_LIBRARY_PATH=~/toolchains/android_prebuilts_clang_host_linux-x86_clang-6443078/lib64:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=~/toolchains/clang-r428724/lib64:$LD_LIBRARY_PATH
 
 echo
 echo "Set DEFCONFIG"
