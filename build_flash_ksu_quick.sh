d5b8a42c4e3b8b3b0f794528bff91d2496031e99
sidex15 <24408329+sidex15@users.noreply.github.com>
2025-03-31 13:30:44 +0800

    switch toolchains to neutron clang

--- build_flash_ksu_quick.sh
+++ build_flash_ksu_quick.sh
@@ -6,8 +6,8 @@ echo
 mkdir -p out
 export ARCH=arm64
 export SUBARCH=arm64
-export CLANG_PATH=~/toolchains/Clang-21.0.0/bin
-#export CLANG_PATH=~/toolchains/weebx-clang16/bin
+#export CLANG_PATH=~/toolchains/neutron-clang/bin
+export CLANG_PATH=~/toolchains/neutron-clang/bin
 export PATH=${CLANG_PATH}:${PATH}
 export CROSS_COMPILE=aarch64-linux-gnu-
 export CROSS_COMPILE_ARM32=arm-linux-gnueabi-
