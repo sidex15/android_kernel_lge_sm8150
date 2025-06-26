dfd0e2d8bbec9e1b5679d955a4b87100e32bc666
sidex15 <24408329+sidex15@users.noreply.github.com>
2025-06-26 12:48:33 +0800

    contain kcal config

--- drivers/gpu/drm/msm/sde/sde_hw_reg_dma_v1_color_proc.c
+++ drivers/gpu/drm/msm/sde/sde_hw_reg_dma_v1_color_proc.c
@@ -1047,7 +1047,9 @@ void reg_dmav1_setup_dspp_pccv4(struct sde_hw_dspp *ctx, void *cfg)
 	struct sde_hw_reg_dma_ops *dma_ops;
 	struct sde_reg_dma_kickoff_cfg kick_off;
 	struct sde_hw_cp_cfg *hw_cfg = cfg;
+	#ifdef CONFIG_DRM_MSM_KCAL_CTRL
 	struct sde_hw_kcal *kcal = sde_hw_kcal_get();
+	#endif
 	struct sde_reg_dma_setup_ops_cfg dma_write_cfg;
 	struct drm_msm_pcc *pcc_cfg;
 	struct drm_msm_pcc_coeff *coeffs = NULL;
@@ -1120,9 +1122,10 @@ void reg_dmav1_setup_dspp_pccv4(struct sde_hw_dspp *ctx, void *cfg)
 		data[i + 6] = coeffs->g;
 		data[i + 9] = coeffs->b;
 
+		#ifdef CONFIG_DRM_MSM_KCAL_CTRL
 		if (kcal->enabled)
 			sde_hw_kcal_pcc_adjust(data, i);
-
+		#endif
 		data[i + 12] = coeffs->rg;
 		data[i + 15] = coeffs->rb;
 		data[i + 18] = coeffs->gb;
@@ -1154,9 +1157,10 @@ void reg_dmav1_setup_dspp_pccv4(struct sde_hw_dspp *ctx, void *cfg)
 	rc = dma_ops->kick_off(&kick_off);
 	if (rc)
 		DRM_ERROR("failed to kick off ret %d\n", rc);
-
+	#ifdef CONFIG_DRM_MSM_KCAL_CTRL
 	if (kcal->enabled)
 		reg_dmav1_setup_dspp_pa_hsicv17_kcal(ctx, hw_cfg->ctl);
+	#endif
 exit:
 	kfree(data);
 }
@@ -1169,11 +1173,15 @@ void reg_dmav1_setup_dspp_pa_hsicv17(struct sde_hw_dspp *ctx, void *cfg)
 	struct sde_reg_dma_setup_ops_cfg dma_write_cfg;
 	struct drm_msm_pa_hsic *hsic_cfg;
 	u32 reg = 0, opcode = 0, local_opcode = 0;
+	#ifdef CONFIG_DRM_MSM_KCAL_CTRL
 	struct sde_hw_kcal *kcal = sde_hw_kcal_get();
+	#endif
 	int rc;
 
+	#ifdef CONFIG_DRM_MSM_KCAL_CTRL
 	if (kcal->enabled)
 		return;
+	#endif
 
 	opcode = SDE_REG_READ(&ctx->hw, ctx->cap->sblk->hsic.base);
 
